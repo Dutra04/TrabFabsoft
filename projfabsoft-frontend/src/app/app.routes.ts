fc39ad13d324ea61b50db2c110cff9eb6d18f9cd
Felipe Dalçóquio <127154872+felipedalcoquio@users.noreply.github.com>
2025-06-10 00:54:23 +0000

    feat

--- projfabsoft-frontend/src/app/app.routes.ts
+++ projfabsoft-frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { ClienteComponent } from './cliente/cliente.component';
-import { Component } from '@angular/core';
+import { FormClienteComponent } from './form-cliente/form-cliente.component';
 
 export const routes: Routes = [
-    {path: 'clientes', component: ClienteComponent}
+    {path: 'clientes', component: ClienteComponent},
+    {path: 'clientes/novo', component: FormClienteComponent},
 ];
